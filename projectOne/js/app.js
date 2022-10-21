a4484dc49061511772f1e44dc48e655d0d6829d8
nateosborne <natedresdeno@gmail.com>
2022-10-20 22:21:55 -0400

    final

--- projectOne/js/app.js
+++ projectOne/js/app.js
@@ -157,141 +157,141 @@ function card(){
 
 //BLUE SECTION
 
-// let piecenumB = 0;
-// let homenumB = 0;
-// let boardspacenumB = 6;
-// let boardspacenumB2 = 6;
-// let boardspacenumB3 = 6;
-// let boardspacenumB4 = 6;
+let piecenumB = 0;
+let homenumB = 0;
+let boardspacenumB = 6;
+let boardspacenumB2 = 6;
+let boardspacenumB3 = 6;
+let boardspacenumB4 = 6;
 
-// //picks up card
-// function cardB(){
-//     let cardnum = Math.floor(Math.random() * 10) + 1;
-//     let cards = [cardnum];
-//     document.getElementById("newcard").style.backgroundColor = "gray";
-//     document.getElementById("newcard").style.height = "300px";
-//     document.getElementById("newcard").style.width = "200px";
-//     document.getElementById("newcard").innerHTML = [cards];
+//picks up card
+function cardB(){
+    let cardnum = Math.floor(Math.random() * 10) + 1;
+    let cards = [cardnum];
+    document.getElementById("newcard").style.backgroundColor = "gray";
+    document.getElementById("newcard").style.height = "300px";
+    document.getElementById("newcard").style.width = "200px";
+    document.getElementById("newcard").innerHTML = [cards];
 
-//     //moves pieces from start
-//     //moves 1st piece from start
-//     document.getElementById("movestart").onclick = function(){
-//     if (piecenumB == 0){
-//         if (cardnum == 2 || cardnum == 1){
-//             document.getElementById("bluepiece1").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('bluepiece1'));
-//             document.getElementById('boardspace6').appendChild(fragment);
-//             piecenumB = 1;
-//             console.log(piecenumB);
-//             }
-//         }
-//     }
-//     //moves 2nd piece from start
-//     if (piecenumB == 1){
-//         if (cardnum == 2 || cardnum == 1){
-//             document.getElementById("bluepiece2").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('bluepiece2'));
-//             document.getElementById('boardspace6').appendChild(fragment);
-//             piecenumB = 2;
-//             console.log(piecenumB);
-//             }
-//         }
-//     }
-//     //moves 3rd piece from start
-//     if (piecenumB == 2){
-//         if (cardnum == 2 || cardnum == 1){
-//             document.getElementById("bluepiece3").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('bluepiece3'));
-//             document.getElementById('boardspace6').appendChild(fragment);
-//             piecenumB = 3;
-//             console.log(piecenumB);
-//             }
-//         }
-//     }
-//     //moves 4th piece from start
-//     if (piecenumB == 3){
-//         if (cardnum == 2 || cardnum == 1){
-//             document.getElementById("bluepiece4").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('bluepiece4'));
-//             document.getElementById('boardspace6').appendChild(fragment);
-//             piecenumB = 4;
-//             console.log(piecenumB);
-//             }
-//         }
-//     }
-// }
+    //moves pieces from start
+    //moves 1st piece from start
+    document.getElementById("movestart").onclick = function(){
+    if (piecenumB == 0){
+        if (cardnum == 2 || cardnum == 1){
+            document.getElementById("bluepiece1").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('bluepiece1'));
+            document.getElementById('boardspace6').appendChild(fragment);
+            piecenumB = 1;
+            console.log(piecenumB);
+            }
+        }
+    }
+    //moves 2nd piece from start
+    if (piecenumB == 1){
+        if (cardnum == 2 || cardnum == 1){
+            document.getElementById("bluepiece2").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('bluepiece2'));
+            document.getElementById('boardspace6').appendChild(fragment);
+            piecenumB = 2;
+            console.log(piecenumB);
+            }
+        }
+    }
+    //moves 3rd piece from start
+    if (piecenumB == 2){
+        if (cardnum == 2 || cardnum == 1){
+            document.getElementById("bluepiece3").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('bluepiece3'));
+            document.getElementById('boardspace6').appendChild(fragment);
+            piecenumB = 3;
+            console.log(piecenumB);
+            }
+        }
+    }
+    //moves 4th piece from start
+    if (piecenumB == 3){
+        if (cardnum == 2 || cardnum == 1){
+            document.getElementById("bluepiece4").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('bluepiece4'));
+            document.getElementById('boardspace6').appendChild(fragment);
+            piecenumB = 4;
+            console.log(piecenumB);
+            }
+        }
+    }
+}
     
-//     document.getElementById("movespaces").onclick = function(){
-//         document.getElementById("bluepiece1").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('bluepiece1'));
-//             boardspacenumB += cardnum;
-//             if (boardspacenumB >= 5){
-//                 document.getElementById('boardspace'+boardspacenumB).appendChild(fragment);
-//             }
-//             if (boardspacenumB >= 25){
-//                 boardspacenumB = cardnum;
-//                 document.getElementById('boardspace'+boardspacenumB).appendChild(fragment);
-//             }
-//             else if(boardspacenumB >= 26){
-//                 document.getElementById('boardspaceblue'+boardspacenumB).appendChild(fragment);
-//             }
-//             else {
-//                 document.getElementById('boardspaceblue').appendChild(fragment);
-//                 homenumB = 1;
-//             }
-//         }
-//         document.getElementById("redpiece2").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('redpiece2'));
-//             boardspacenum2 += cardnum;
-//             if (boardspacenum2 <= 23){
-//                 document.getElementById('boardspace'+boardspacenum2).appendChild(fragment);
-//             }
-//             else if(boardspacenum2 <= 26){
-//                 document.getElementById('boardspacered'+boardspacenum2).appendChild(fragment);
-//             }
-//             else {
-//                 document.getElementById('boardspacered27').appendChild(fragment);
-//                 homenum = 2;
-//             }
-//         }
-//         document.getElementById("redpiece3").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('redpiece3'));
-//             boardspacenum3 += cardnum;
-//             if (boardspacenum3 <= 23){
-//                 document.getElementById('boardspace'+boardspacenum3).appendChild(fragment);
-//             }
-//             else if(boardspacenum3 <= 26){
-//                 document.getElementById('boardspacered'+boardspacenum3).appendChild(fragment);
-//             }
-//             else {
-//                 document.getElementById('boardspacered27').appendChild(fragment);
-//                 homenum = 3;
-//             }
-//         }
-//         document.getElementById("redpiece4").onclick = function() {
-//             var fragment = document.createDocumentFragment();
-//             fragment.appendChild(document.getElementById('redpiece4'));
-//             boardspacenum4 += cardnum;
-//             if (boardspacenum4 <= 23){
-//                 document.getElementById('boardspace'+boardspacenum4).appendChild(fragment);
-//             }
-//             else if(boardspacenum4 <= 26){
-//                 document.getElementById('boardspacered'+boardspacenum4).appendChild(fragment);
-//             }
-//             else {
-//                 document.getElementById('boardspacered27').appendChild(fragment);
-//                 homenum = 4;
-//                 document.getElementById("win").innerHTML = "YOU WIN!";
-//                 document.getElementById("win").style.fontSize = "100px";
-//             }
-//         }
+    document.getElementById("movespaces").onclick = function(){
+        document.getElementById("bluepiece1").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('bluepiece1'));
+            boardspacenumB += cardnum;
+            if (boardspacenumB >= 5){
+                document.getElementById('boardspace'+boardspacenumB).appendChild(fragment);
+            }
+            if (boardspacenumB >= 25){
+                boardspacenumB = cardnum;
+                document.getElementById('boardspace'+boardspacenumB).appendChild(fragment);
+            }
+            else if(boardspacenumB >= 26){
+                document.getElementById('boardspaceblue'+boardspacenumB).appendChild(fragment);
+            }
+            else {
+                document.getElementById('boardspaceblue').appendChild(fragment);
+                homenumB = 1;
+            }
+        }
+        document.getElementById("redpiece2").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('redpiece2'));
+            boardspacenum2 += cardnum;
+            if (boardspacenum2 <= 23){
+                document.getElementById('boardspace'+boardspacenum2).appendChild(fragment);
+            }
+            else if(boardspacenum2 <= 26){
+                document.getElementById('boardspacered'+boardspacenum2).appendChild(fragment);
+            }
+            else {
+                document.getElementById('boardspacered27').appendChild(fragment);
+                homenum = 2;
+            }
+        }
+        document.getElementById("redpiece3").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('redpiece3'));
+            boardspacenum3 += cardnum;
+            if (boardspacenum3 <= 23){
+                document.getElementById('boardspace'+boardspacenum3).appendChild(fragment);
+            }
+            else if(boardspacenum3 <= 26){
+                document.getElementById('boardspacered'+boardspacenum3).appendChild(fragment);
+            }
+            else {
+                document.getElementById('boardspacered27').appendChild(fragment);
+                homenum = 3;
+            }
+        }
+        document.getElementById("redpiece4").onclick = function() {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(document.getElementById('redpiece4'));
+            boardspacenum4 += cardnum;
+            if (boardspacenum4 <= 23){
+                document.getElementById('boardspace'+boardspacenum4).appendChild(fragment);
+            }
+            else if(boardspacenum4 <= 26){
+                document.getElementById('boardspacered'+boardspacenum4).appendChild(fragment);
+            }
+            else {
+                document.getElementById('boardspacered27').appendChild(fragment);
+                homenum = 4;
+                document.getElementById("win").innerHTML = "YOU WIN!";
+                document.getElementById("win").style.fontSize = "100px";
+            }
+        }
 
-// }
-// }
+}
+}
